77cb41ddf415b8b6069a4ee4f903e9a56dbdc4b9
Young Jorisky <jojoyoung3@gmail.com>
2026-01-06 17:54:00 +0000

    Append division elements by json

--- track.js
+++ track.js
@@ -1,9 +1,40 @@
-async function getData() {
-  try {
-    const data = await fetch("data/json");
-    const jsonData = await data.json();
-    return jsonData;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+const trackDivisions = document.getElementById("time-track-divisions");
+const button = document.querySelectorAll(".lower-identity h4");
+
+let data = [];
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((dataFile) => {
+    data = dataFile;
+    displayItems("daily");
+  });
+
+function displayItems(type) {
+  trackDivisions.innerHTML = "";
+
+  data.forEach((item) => {
+    const itemDiv = document.createElement("div");
+
+    const className = item.title.toLowerCase().replace(" ", "-");
+    itemDiv.classList.add(className, "division");
+
+    itemDiv.innerHTML = `
+          <div class="icon-container">
+            <img src="./images/icon-${className}.svg" />
+          </div>
+          <div class="text-container">
+            <div class="left-side">
+              <h4>${item.title}</h4>
+              <h4>...</h4>
+            </div>
+            <div class="right-side">
+              <h2>${item.timeframes[type].current}hrs</h2>
+              <p>Last Week - ${item.timeframes[type].previous}hrs</p>
+            </div>
+          </div>
+        `;
+
+    trackDivisions.appendChild(itemDiv);
+  });
 }
